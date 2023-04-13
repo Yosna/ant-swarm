7d7d09e1ff45c5cb40520010e47c586704511dfe
Yosna <34321125+Yosna@users.noreply.github.com>
2023-04-12 23:18:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let species = {
+let antSpecies = {
     garden: {
-        smallWorker: {name: "Small Garden Worker", id: "small-garden-worker"},
-        regularWorker: {name: "Regular Garden Worker", id: "regular-garden-worker"},
-        largeWorker: {name: "Large Garden Worker", id: "large-garden-worker"},
-        smallDrone: {name: "Small Garden Drone", id: "small-garden-drone"},
-        regularDrone: {name: "Regular Garden Drone", id: "regular-garden-drone"},
-        largeDrone: {name: "Large Garden Drone", id: "large-garden-drone"},
-        queen: {name: "Garden Ant Queen", id: "garden-ant-queen"},
+        smallWorker: {name: "Small Garden Worker", id: "small-garden-worker", product: "food", abb: "F/s"},
+        regularWorker: {name: "Regular Garden Worker", id: "regular-garden-worker", product: "small garden workers", abb: "SGW/s"},
+        largeWorker: {name: "Large Garden Worker", id: "large-garden-worker", product: "regular garden workers", abb: "RGW/s"},
+        smallDrone: {name: "Small Garden Drone", id: "small-garden-drone", product: "large garden workers", abb: "LGW/s"},
+        regularDrone: {name: "Regular Garden Drone", id: "regular-garden-drone", product: "small garden drones", abb: "SGD/s"},
+        largeDrone: {name: "Large Garden Drone", id: "large-garden-drone", product: "regular garden drones", abb: "RGD/s"},
+        queen: {name: "Garden Ant Queen", id: "garden-ant-queen", product: "large garden drones", abb: "LGD/s"},
     },
 };
 
@@ -20,15 +20,15 @@ let resources = {
 let foragingRate = 1;
 let foragingBoost = 1;
 
-for (let [i, ants] of Object.values(species).entries()) {
-    for (let [n, ant] of Object.values(ants).entries()) {
+for (let [type, ants] of Object.values(antSpecies).entries()) {
+    for (let [tier, ant] of Object.values(ants).entries()) {
         ant.bought = 0;
         ant.owned = 0;
         ant.production = .1;
         ant.boost = 1;
     };
-    for (let [n, ant] of Object.values(ants).entries()) {
-        ant.cost = (1 * Math.pow(10, n * 2)) * Math.pow(1.12, ant.bought);
+    for (let [tier, ant] of Object.values(ants).entries()) {
+        ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought);
     };
 };
 
@@ -37,16 +37,15 @@ function forage() {
     document.getElementById("food-total").innerHTML = numberFormat(resources.food.total);
 };
 
-function recruit(type, tier) {
-    for (let [i, ants] of Object.values(species).entries()) {
-        if (i == type) {
-            for (let [n, ant] of Object.values(ants).entries()) {
-                if (n == tier) {
+function recruit(antType, antTier) {
+    for (let [type, ants] of Object.values(antSpecies).entries()) {
+        if (antType == type) {
+            for (let [tier, ant] of Object.values(ants).entries()) {
+                if (antTier == tier) {
                     if (ant.cost > resources.food.total) {
                         return;
                     } else {
                         resources.food.total -= ant.cost;
-                        resources.food.production += ant.production;
                         ant.bought++;
                         ant.owned++;
                     }
@@ -78,24 +77,45 @@ function updateDisplay() {
     document.getElementById("food-total").innerHTML = numberFormat(resources.food.total);
     document.getElementById("food-production").innerHTML = numberFormat(resources.food.production);
 
-    for (let [i, ants] of Object.values(species).entries()) {
-        for (let [n, ant] of Object.values(ants).entries()) {
-            ant.cost = (1 * Math.pow(10, n * 2)) * Math.pow(1.12, ant.bought);
+    for (let [type, ants] of Object.values(antSpecies).entries()) {
+        for (let [tier, ant] of Object.values(ants).entries()) {
             document.getElementById(ant.id + "-bought").innerHTML = numberFormat(ant.bought);
             document.getElementById(ant.id + "-owned").innerHTML = numberFormat(ant.owned);
+            document.getElementById(ant.id + "-production").innerHTML = numberFormat(ant.production * ant.owned) + ' ' + ant.abb;
             document.getElementById(ant.id + "-cost").innerHTML = numberFormat(ant.cost);
-            document.getElementById(ant.id + "-production").innerHTML = numberFormat(ant.production * ant.owned);
+            
+            if ((ant.owned == 0) && (resources.food.total < (ant.cost / 2))) {
+                document.getElementById(ant.id + "-button").style.display = "none";
+                document.getElementById(ant.id + "-data-1").style.display = "none";
+                document.getElementById(ant.id + "-data-2").style.display = "none";
+            } else {
+                document.getElementById(ant.id + "-button").style.display = "";
+                document.getElementById(ant.id + "-data-1").style.display = "";
+                document.getElementById(ant.id + "-data-2").style.display = "";
+            };
         };
     };
 };
 
 setInterval(function gameLoop() {
-    for (let [i, ants] of Object.values(species).entries()) {
-        for (let [n, ant] of Object.values(ants).entries()) {
-            if (n == 0) {
-                resources.food.total += ((ant.production * ant.owned) / 10);
-            }
+    let totalFoodProduction = 0;
+
+    for (let [type, ants] of Object.values(antSpecies).entries()) {
+        for (let [tier, ant] of Object.values(ants).entries()) {
+            if (tier == 0) {
+                // Add food if the ant is the lowest tier
+                resources.food.total += (ant.production * ant.owned) / 10;
+                totalFoodProduction += ant.production * ant.owned;
+            } else {
+                // Add ants to the tier below
+                Object.values(antSpecies)[type][Object.keys(ants)[tier - 1]].owned += ((ant.production * ant.owned) / 10);
+            };
+            // Calculate the current cost of the ant in the tier
+            ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought);
         };
     };
+
+    resources.food.production = totalFoodProduction;
+
     updateDisplay();
 }, 100);
