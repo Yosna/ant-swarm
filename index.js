6f146a8704e35db9c93830e51518782fe40b2791
Yosna <34321125+Yosna@users.noreply.github.com>
2023-04-16 22:26:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,99 +20,129 @@ let resources = {
 let foragingRate = 1;
 let foragingBoost = 1;
 
-for (let [type, ants] of Object.values(antSpecies).entries()) {
-    for (let [tier, ant] of Object.values(ants).entries()) {
-        ant.bought = 0;
-        ant.owned = 0;
-        ant.production = 0.1;
-        ant.boost = 1;
-    };
-    for (let [tier, ant] of Object.values(ants).entries()) {
-        ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought);
-    };
-};
+const game = (() => {
 
-function forage() {
-    resources.food.total += (foragingRate * foragingBoost);
-    document.getElementById("food-total").innerHTML = numberFormat(resources.food.total);
-};
-
-function recruit(antType, antTier) {
-    for (let [type, ants] of Object.values(antSpecies).entries()) {
-        if (antType == type) {
-            for (let [tier, ant] of Object.values(ants).entries()) {
-                if (antTier == tier) {
-                    if (ant.cost > resources.food.total) {
-                        return;
-                    } else {
-                        resources.food.total -= ant.cost;
-                        ant.bought++;
-                        ant.owned++;
-                    }
-                };
-            };
-        };
-    };
-};
-
-function numberFormat(n) {
-    if (n < 100) {
-        return parseFloat(n.toFixed(2));
-    } else if (n < 1000) {
-        return parseFloat(n.toFixed(1));
-    } else if (n < 10000) {
-        return parseFloat(n.toFixed(1)).toLocaleString();
-    } else {
-        const f = new Intl.NumberFormat("en-US", {
-            notation: "scientific", 
-            minimumSignificantDigits: 4, 
-            maximumSignificantDigits: 4,
-            roundingMode: "trunc",
-        });
-        return f.format(n);
-    };
-};
-
-function updateDisplay() {
-    document.getElementById("food-total").innerHTML = numberFormat(resources.food.total);
-    document.getElementById("food-production").innerHTML = numberFormat(resources.food.production);
-
+    // Set the values for all ants
     for (let [type, ants] of Object.values(antSpecies).entries()) {
         for (let [tier, ant] of Object.values(ants).entries()) {
-
-            const displayCostThreshold = (ant.owned == 0) && (resources.food.total < (ant.cost / 2));
-            let display = displayCostThreshold ? "none" : "";
-            document.getElementById(ant.id + "-button").style.display = display;
-            document.getElementById(ant.id + "-data-1").style.display = display;
-            document.getElementById(ant.id + "-data-2").style.display = display;
-
-            document.getElementById(ant.id + "-bought").innerHTML = numberFormat(ant.bought);
-            document.getElementById(ant.id + "-owned").innerHTML = numberFormat(ant.owned);
-            document.getElementById(ant.id + "-production").innerHTML = numberFormat(ant.production * ant.owned) + ' ' + ant.abb;
-            document.getElementById(ant.id + "-cost").innerHTML = numberFormat(ant.cost);
+            ant.bought = 0;
+            ant.owned = 0;
+            ant.production = .1;
+            ant.boost = 1;
         };
-    };
-};
-
-setInterval(function gameLoop() {
-    let totalFoodProduction = 0;
-
-    for (let [type, ants] of Object.values(antSpecies).entries()) {
         for (let [tier, ant] of Object.values(ants).entries()) {
-            if (tier == 0) {
-                // Add food if the ant is the lowest tier
-                resources.food.total += (ant.production * ant.owned) / 10;
-                totalFoodProduction += ant.production * ant.owned;
-            } else {
-                // Add ants to the tier below
-                Object.values(antSpecies)[type][Object.keys(ants)[tier - 1]].owned += ((ant.production * ant.owned) / 10);
-            };
-            // Calculate the current cost of the ant in the tier
             ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought);
         };
     };
 
-    resources.food.production = totalFoodProduction;
+    // Recruit an ant
+    function recruit(antType, antTier) {
+        for (let [type, ants] of Object.values(antSpecies).entries()) {
+            if (antType == type) {
+                for (let [tier, ant] of Object.values(ants).entries()) {
+                    if (antTier == tier) {
+                        if (ant.cost > resources.food.total) {
+                            return;
+                        } else {
+                            resources.food.total -= ant.cost;
+                            ant.bought++;
+                            ant.owned++;
+                        }
+                    };
+                };
+            };
+        };
+    };
 
-    updateDisplay();
-}, 100);
+    function forage() {
+        resources.food.total += (foragingRate * foragingBoost);
+        document.getElementById("food-total").innerHTML = utility.numberFormat(resources.food.total);
+    };
+
+    return {
+        recruit,
+        forage,
+    }
+})();
+
+const utility = (() => {
+
+    // Number handling
+    const scientificNotation = new Intl.NumberFormat("en-US", {
+        notation: "scientific",
+        minimumSignificantDigits: 4,
+        maximumSignificantDigits: 4,
+        roundingMode: "trunc",
+    });
+
+    function numberFormat(n) {
+        // Check the value of the number and return the appropriate format
+        switch (true) {
+            case n < 100: 
+                return parseFloat(n.toFixed(2));
+            case n < 1000: 
+                return parseFloat(n.toFixed(1));
+            case n < 10000: 
+                return parseFloat(n.toFixed(1)).toLocaleString();
+            default: 
+                return scientificNotation.format(n);
+        }
+    };
+
+    // Update the game UI
+    function display() {
+        // Update resource information
+        document.getElementById("food-total").innerHTML = numberFormat(resources.food.total);
+        document.getElementById("food-production").innerHTML = numberFormat(resources.food.production);
+    
+        for (let [type, ants] of Object.values(antSpecies).entries()) {
+            for (let [tier, ant] of Object.values(ants).entries()) {
+    
+                // Determine if the cost threshold has been met to display the next ant
+                const displayCostThreshold = (ant.owned == 0) && (resources.food.total < (ant.cost / 4));
+                let display = displayCostThreshold ? "none" : "";
+                
+                // Update visibility of the ant
+                document.getElementById(ant.id + "-button").style.display = display;
+                document.getElementById(ant.id + "-data-1").style.display = display;
+                document.getElementById(ant.id + "-data-2").style.display = display;
+
+                if (display == "") { // Update the ant information if visible
+                    document.getElementById(ant.id + "-bought").innerHTML = numberFormat(ant.bought);
+                    document.getElementById(ant.id + "-owned").innerHTML = numberFormat(ant.owned);
+                    document.getElementById(ant.id + "-production").innerHTML = numberFormat(ant.production * ant.owned) + ' ' + ant.abb;
+                    document.getElementById(ant.id + "-cost").innerHTML = numberFormat(ant.cost);
+                }
+            };
+        };
+    };
+    
+    // Timer to keep the game updated
+    setInterval(function gameLoop() {
+        let totalFoodProduction = 0;
+    
+        for (let [type, ants] of Object.values(antSpecies).entries()) {
+            for (let [tier, ant] of Object.values(ants).entries()) {
+                if (tier == 0) {
+                    // Add food if the ant is the lowest tier
+                    resources.food.total += (ant.production * ant.owned) / 10;
+                    totalFoodProduction += ant.production * ant.owned;
+                } else {
+                    // Add ants to the tier below
+                    Object.values(antSpecies)[type][Object.keys(ants)[tier - 1]].owned += ((ant.production * ant.owned) / 10);
+                };
+                // Calculate the current cost of the ant in the tier
+                ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought) * (tier + 1);
+            };
+        };
+    
+        resources.food.production = totalFoodProduction;
+    
+        display();
+    }, 100);
+
+    return {
+        numberFormat,
+        display,
+    }
+})();
