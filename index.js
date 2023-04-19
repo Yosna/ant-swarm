316c9cc4cda0d11df8505352ebe5da936264d4ba
Yosna <34321125+Yosna@users.noreply.github.com>
2023-04-19 18:03:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let antSpecies = {
     garden: {
-        smallWorker: {name: "Small Garden Worker", id: "small-garden-worker", idabb: "SGW", product: "food", abb: "F/s"},
-        regularWorker: {name: "Regular Garden Worker", id: "regular-garden-worker", idabb: "RGW", product: "small garden workers", abb: "SGW/s"},
-        largeWorker: {name: "Large Garden Worker", id: "large-garden-worker", idabb: "LGW", product: "regular garden workers", abb: "RGW/s"},
-        smallDrone: {name: "Small Garden Drone", id: "small-garden-drone", idabb: "SGD", product: "large garden workers", abb: "LGW/s"},
-        regularDrone: {name: "Regular Garden Drone", id: "regular-garden-drone", idabb: "RGD", product: "small garden drones", abb: "SGD/s"},
-        largeDrone: {name: "Large Garden Drone", id: "large-garden-drone", idabb: "LGD", product: "regular garden drones", abb: "RGD/s"},
-        queen: {name: "Garden Ant Queen", id: "garden-ant-queen", idabb: "GQ", product: "large garden drones", abb: "LGD/s"},
+        smallWorker: {name: 'Small Garden Worker', id: 'small-garden-worker', id_abb: 'SGW', product: 'food', prod_abb: 'F/s'},
+        regularWorker: {name: 'Regular Garden Worker', id: 'regular-garden-worker', id_abb: 'RGW', product: 'small garden workers', prod_abb: 'SGW/s'},
+        largeWorker: {name: 'Large Garden Worker', id: 'large-garden-worker', id_abb: 'LGW', product: 'regular garden workers', prod_abb: 'RGW/s'},
+        smallDrone: {name: 'Small Garden Drone', id: 'small-garden-drone', id_abb: 'SGD', product: 'large garden workers', prod_abb: 'LGW/s'},
+        regularDrone: {name: 'Regular Garden Drone', id: 'regular-garden-drone', id_abb: 'RGD', product: 'small garden drones', prod_abb: 'SGD/s'},
+        largeDrone: {name: 'Large Garden Drone', id: 'large-garden-drone', id_abb: 'LGD', product: 'regular garden drones', prod_abb: 'RGD/s'},
+        queen: {name: 'Garden Ant Queen', id: 'garden-ant-queen', id_abb: 'GQ', product: 'large garden drones', prod_abb: 'LGD/s'},        
     },
 };
 
@@ -20,25 +20,37 @@ let resources = {
 let foragingRate = 1;
 let foragingBoost = 1;
 
-const game = (() => {
+const init = (() => {
+    
+    function load() {
+        // Set the values for all ants
+        for (let [type, ants] of Object.values(antSpecies).entries()) {
+            for (let [tier, ant] of Object.values(ants).entries()) {
+                ant.bought = 0;
+                ant.owned = 0;
+                ant.production = .1;
+                ant.boost = 1;
+                ant.upgrades = 1;
+            };
+            for (let [tier, ant] of Object.values(ants).entries()) {
+                ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought);
+            };
+        };
 
-    // Set the values for all ants
-    for (let [type, ants] of Object.values(antSpecies).entries()) {
-        for (let [tier, ant] of Object.values(ants).entries()) {
-            ant.bought = 0;
-            ant.owned = 0;
-            ant.production = .1;
-            ant.boost = 1;
-            ant.upgrades = 1;
-        };
-        for (let [tier, ant] of Object.values(ants).entries()) {
-            ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought);
-        };
+        util.timer();
     };
 
-    // Hide locked content
-    for (let upgradeSlot = 1; upgradeSlot < 9; upgradeSlot++) {
-        document.getElementById("upgrade-button-" + upgradeSlot).style.display = "none";
+    return {
+        load,
+    };
+})();
+
+const game = (() => {
+
+    // Gather food by foraging
+    function forage() {
+        resources.food.total += (foragingRate * foragingBoost);
+        document.getElementById('food-total').innerHTML = util.numbers(resources.food.total);
     };
 
     // Recruit an ant
@@ -47,50 +59,117 @@ const game = (() => {
             if (antType == type) {
                 for (let [tier, ant] of Object.values(ants).entries()) {
                     if (antTier == tier) {
-                        if (ant.cost > resources.food.total) {
-                            return; // Not enough food to buy the ant
-                        } else {
-                            resources.food.total -= ant.cost;
-                            ant.bought++;
-                            ant.owned++;
 
-                            // Determine if any upgrade prerequisites have been met
-                            if (ant.bought >= (ant.upgrades * 1)) {
-                                for (let upgradeSlot = 1; upgradeSlot < 9; upgradeSlot++) {
-                                    const upgradeButton = document.getElementById("upgrade-button-" + upgradeSlot);
-                                    
-                                    if (upgradeButton.classList.contains(ant.id + "-upgrade-" + ant.upgrades)) {
-                                        break; // Exit the loop and do nothing if the upgrade is already displayed
-                                    }
-                                    if (upgradeButton.style.display == "none") {
-                                        upgradeButton.setAttribute("class", ant.id + "-upgrade-" + ant.upgrades);
-                                        upgradeButton.innerHTML = ant.idabb + ant.upgrades;
-                                        upgradeButton.style.display = "";
-                                        break;
-                                    };
-                                };
-                            };
-                        };
+                        // Check if the food is sufficient
+                        if (ant.cost > resources.food.total) return;
+
+                        resources.food.total -= ant.cost;
+                        ant.bought++;
+                        ant.owned++;
                     };
                 };
             };
         };
     };
 
-    function forage() {
-        resources.food.total += (foragingRate * foragingBoost);
-        document.getElementById("food-total").innerHTML = utility.numberFormat(resources.food.total);
-    };
+    const calculate = (() => {
+
+        function upgrades() {
+            for (let [type, ants] of Object.values(antSpecies).entries()) {
+                for (let [tier, ant] of Object.values(ants).entries()) {
+
+                    // Determine if any upgrade prerequisites have been met
+                    if (ant.bought >= (ant.upgrades * 1)) {
+                        const upgradeContainer = document.getElementsByClassName('upgrade-button-container')[0];
+                        const upgradesUnlocked = upgradeContainer.querySelectorAll('*');
+    
+                        // Check if the upgrade is already unlocked
+                        for (let i = 0; i < (upgradesUnlocked.length); i++) {
+                            if (upgradesUnlocked[i].id == ant.id_abb) return; 
+                        };
+                        
+                        // Create a new upgrade button once the prerequisites have been met
+                        const buttonElement = ('<button type="button" class="upgrade-button" id="' + ant.id_abb + '" onclick=game.upgrade()">' + ant.id_abb + '</button>');
+
+                        upgradeContainer.innerHTML += buttonElement; // Display the element
+                    };
+                };
+            };
+        };
+
+        function resourceProduction() {
+            let totalFoodProduction = 0;
+        
+            for (let [type, ants] of Object.values(antSpecies).entries()) {
+                for (let [tier, ant] of Object.values(ants).entries()) {
+                    if (tier == 0) { 
+                        // Add food if the ant is the lowest tier
+                        resources.food.total += (ant.production * ant.owned) / 10;
+                        totalFoodProduction += ant.production * ant.owned;
+                    } else { 
+                        // Add ants to the tier below
+                        Object.values(antSpecies)[type][Object.keys(ants)[tier - 1]].owned += ((ant.production * ant.owned) / 10);
+                    };
+                    // Calculate the cost of the ant for the current tier
+                    ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought) * (tier + 1);
+                };
+            };
+        
+            resources.food.production = totalFoodProduction;
+        };
+
+        return {
+            upgrades,
+            resourceProduction,
+        }
+    })();
+
+    const display = (() => {
+
+        // Update the game UI
+        function update() {
+            // Update resource information
+            document.getElementById('food-total').innerHTML = util.numbers(resources.food.total);
+            document.getElementById('food-production').innerHTML = util.numbers(resources.food.production);
+        
+            for (let [type, ants] of Object.values(antSpecies).entries()) {
+                for (let [tier, ant] of Object.values(ants).entries()) {
+
+                    // Determine if the cost threshold has been met to display the next ant
+                    const displayCostThreshold = (ant.owned == 0) && (resources.food.total < (ant.cost / 4));
+                    let display = displayCostThreshold ? 'none' : '';
+                    
+                    // Update visibility of the ant's information
+                    document.getElementById(ant.id + '-button').style.display = display;
+                    document.getElementById(ant.id + '-data-1').style.display = display;
+                    document.getElementById(ant.id + '-data-2').style.display = display;
+    
+                    // Update the ant's display if visible
+                    if (display == "") {
+                        document.getElementById(ant.id + '-bought').innerHTML = util.numbers(ant.bought);
+                        document.getElementById(ant.id + '-owned').innerHTML = util.numbers(ant.owned);
+                        document.getElementById(ant.id + '-production').innerHTML = util.numbers(ant.production * ant.owned) + ' ' + ant.prod_abb;
+                        document.getElementById(ant.id + '-cost').innerHTML = util.numbers(ant.cost);
+                    }
+                };
+            };
+        };
+
+        return {
+            update,
+        }
+    })();
 
     return {
-        recruit,
         forage,
-    }
+        recruit,
+        calculate,
+        display,
+    };
 })();
 
-const utility = (() => {
+const util = (() => {
 
-    // Number handling
     const scientificNotation = new Intl.NumberFormat("en-US", {
         notation: "scientific",
         minimumSignificantDigits: 4,
@@ -98,7 +177,8 @@ const utility = (() => {
         roundingMode: "trunc",
     });
 
-    function numberFormat(n) {
+    // Number format handling
+    function numbers(n) {
         // Check the value of the number and return the appropriate format
         switch (true) {
             case n < 100: 
@@ -111,61 +191,24 @@ const utility = (() => {
                 return scientificNotation.format(n);
         }
     };
-
-    // Update the game UI
-    function display() {
-        // Update resource information
-        document.getElementById("food-total").innerHTML = numberFormat(resources.food.total);
-        document.getElementById("food-production").innerHTML = numberFormat(resources.food.production);
     
-        for (let [type, ants] of Object.values(antSpecies).entries()) {
-            for (let [tier, ant] of Object.values(ants).entries()) {
-    
-                // Determine if the cost threshold has been met to display the next ant
-                const displayCostThreshold = (ant.owned == 0) && (resources.food.total < (ant.cost / 4));
-                let display = displayCostThreshold ? "none" : "";
-                
-                // Update visibility of the ant
-                document.getElementById(ant.id + "-button").style.display = display;
-                document.getElementById(ant.id + "-data-1").style.display = display;
-                document.getElementById(ant.id + "-data-2").style.display = display;
+    // Create a timer to keep the game running
+    function timer() {
+        setInterval(function gameCycle() {
 
-                if (display == "") { // Update the ant information if visible
-                    document.getElementById(ant.id + "-bought").innerHTML = numberFormat(ant.bought);
-                    document.getElementById(ant.id + "-owned").innerHTML = numberFormat(ant.owned);
-                    document.getElementById(ant.id + "-production").innerHTML = numberFormat(ant.production * ant.owned) + ' ' + ant.abb;
-                    document.getElementById(ant.id + "-cost").innerHTML = numberFormat(ant.cost);
-                }
-            };
-        };
+            game.calculate.upgrades();
+
+            game.calculate.resourceProduction();
+
+            game.display.update();
+
+        }, 100);
     };
-    
-    // Timer to keep the game updated
-    setInterval(function gameLoop() {
-        let totalFoodProduction = 0;
-    
-        for (let [type, ants] of Object.values(antSpecies).entries()) {
-            for (let [tier, ant] of Object.values(ants).entries()) {
-                if (tier == 0) {
-                    // Add food if the ant is the lowest tier
-                    resources.food.total += (ant.production * ant.owned) / 10;
-                    totalFoodProduction += ant.production * ant.owned;
-                } else {
-                    // Add ants to the tier below
-                    Object.values(antSpecies)[type][Object.keys(ants)[tier - 1]].owned += ((ant.production * ant.owned) / 10);
-                };
-                // Calculate the current cost of the ant in the tier
-                ant.cost = (1 * Math.pow(10, tier * 2)) * Math.pow(1.12, ant.bought) * (tier + 1);
-            };
-        };
-    
-        resources.food.production = totalFoodProduction;
-    
-        display();
-    }, 100);
 
     return {
-        numberFormat,
-        display,
-    }
+        numbers,
+        timer,
+    };
 })();
+
+window.onload = init.load();
