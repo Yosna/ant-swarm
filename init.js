e5a3595a35825105812fec1b86adc0b349487620
Yosna <kodyj123@gmail.com>
2023-05-11 18:13:52 -0400

    refactoring

--- init.js
+++ init.js
@@ -1,16 +1,74 @@
 import { recruits, resources, stats, conditions } from './index.js';
 import * as game from './game.js';
+import { getElement } from './util.js';
 import { updateElement } from './display.js';
+
+const eventListeners = getEventListeners({
+    gameEvents: {
+        forage: eventListener('#forage-button', 'click', game.forage),
+        recruit: eventListener('.recruit-button', 'click', e => {
+            game.recruit(e.target);
+        })
+    },
+    menuEvents: {
+        importExport: eventListener('.import-export-toggle', 'click', game.display.modals.importExport),
+        settings: eventListener('.settings-toggle', 'click', game.display.modals.settings),
+        stats: eventListener('.stats-toggle', 'click', game.display.modals.stats)
+    },
+    saveEvents: {
+        save: eventListener('#save-button', 'click', game.util.save),
+        del: eventListener('#delete-button', 'click', game.util.deleteSave),
+        import: eventListener('#import-button', 'click', game.util.importSave),
+        export: eventListener('.export-button', 'click', e => {
+            game.util.exportSave(e.target.innerHTML);
+        })
+    },
+    settingsEvents: {
+        autoSave: eventListener('#autosave-button', 'click', game.util.toggleAutoSave),
+        quantity: eventListener('#quantity-selection', 'change', e => {
+            for (const { ant } of game.getAnts()) {
+                game.calculate.costByQuantity(ant);
+                e.target.blur();
+            }
+        }),
+        rounding: eventListener('#rounding-button', 'click', game.util.toggleRounding),
+        clearLog: eventListener('#clear-log-button', 'click', game.util.clearLogs)
+    },
+    utilityEvents: {
+        visibility: eventListener(null, 'visibilitychange', () => {
+            conditions.activeWindow = !conditions.activeWindow;
+            if (conditions.activeWindow) {
+                game.calculate.offlineProgress();
+            }
+        }),
+        scroll: eventListener('.upgrade-button-container', 'wheel', e => {
+            if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+                e.preventDefault();
+                const target = e.target.tagName === 'DIV'
+                    ? e.target
+                    : e.target.parentElement;
+                target.scrollLeft += e.deltaY;
+            }
+        }),
+        modals: {
+            settings: eventListener(null, 'click', e => {
+                hideIfNotActive('#settings-modal', e.target, 'settings-toggle');
+            }),
+            stats: eventListener(null, 'click', e => {
+                hideIfNotActive('#stats-modal', e.target, 'stats-toggle');
+            }),
+            importExport: eventListener(null, 'click', e => {
+                hideIfNotActive('#import-export-modal', e.target, 'import-export-toggle');
+            })
+        }
+    }
+});
 
 function load() {
     const saveFound = localStorage.getItem('saveData');
     saveFound ? getSave(saveFound) : newSave();
     setElements();
-    gameEventListeners();
-    menuEventListeners();
-    saveEventListeners();
-    settingsEventListeners();
-    utilityEventListeners();
+    setEventListeners();
 }
 
 function getSave(encodedData) {
@@ -21,7 +79,6 @@ function getSave(encodedData) {
         Object.assign(resources, saveData.resources);
         Object.assign(stats, saveData.stats);
         Object.assign(conditions, saveData.conditions);
-
         game.calculate.offlineProgress();
         return true;
     } catch (error) {
@@ -50,6 +107,7 @@ function newSave() {
 }
 
 function setElements() {
+    getElement('#creation-date').innerHTML = stats.creationDate;
     for (const { ant } of game.getAnts()) {
         if (ant.unlocked) {
             updateElement(`.${ant.id}-data`, 'style.visibility', 'visible');
@@ -60,6 +118,22 @@ function setElements() {
     game.util.toggleAutoSave();
     game.util.toggleRounding();
     game.util.setTimers();
+}
+
+function * getEventListeners() {
+    for (const event in eventListeners) {
+        if (typeof eventListeners[event] === 'function') {
+            yield eventListeners[event];
+        } else {
+            yield * getEventListeners(eventListeners[event]);
+        }
+    }
+}
+
+function setEventListeners() {
+    for (const eventListener of getEventListeners()) {
+        eventListener();
+    }
 }
 
 function eventListener(selector, event, callback) {
@@ -73,58 +147,14 @@ function eventListener(selector, event, callback) {
     }
 }
 
-function gameEventListeners() {
-    eventListener('#forage-button', 'click', game.forage);
-    eventListener('.recruit-button', 'click', e => {
-        game.recruit(e.target);
-    });
-}
-
-function menuEventListeners() {
-    eventListener('.import-export-toggle', 'click', game.display.importExportModal);
-    eventListener('.settings-toggle', 'click', e => {
-        const display = e.target.innerText === 'Settings';
-        game.display.settings(display);
-    });
-}
-
-function saveEventListeners() {
-    eventListener('#save-button', 'click', game.util.save);
-    eventListener('#delete-button', 'click', game.util.deleteSave);
-    eventListener('#import-button', 'click', game.util.importSave);
-    eventListener('.export-button', 'click', e => {
-        game.util.exportSave(e.target.innerHTML);
-    });
-}
-
-function settingsEventListeners() {
-    eventListener('#autosave-button', 'click', game.util.toggleAutoSave);
-    eventListener('#quantity-selection', 'change', e => {
-        for (const { ant } of game.getAnts()) {
-            game.calculate.costByQuantity(ant);
-            e.target.blur();
-        }
-    });
-    eventListener('#rounding-button', 'click', game.util.toggleRounding);
-    eventListener('#clear-log-button', 'click', game.util.clearLogs);
-}
-
-function utilityEventListeners() {
-    eventListener(null, 'visibilitychange', () => {
-        conditions.activeWindow = !conditions.activeWindow;
-        if (conditions.activeWindow) {
-            game.calculate.offlineProgress();
-        }
-    });
-    eventListener('.upgrade-button-container', 'wheel', e => {
-        if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-            e.preventDefault();
-            const target = e.target.tagName === 'DIV'
-                ? e.target
-                : e.target.parentElement;
-            target.scrollLeft += e.deltaY;
-        }
-    });
+function hideIfNotActive(selector, target, exception) {
+    const element = $(selector);
+    if (!$(target).is(element) &&
+        !element.has(target).length &&
+        target.className !== exception
+    ) {
+        element.hide();
+    }
 }
 
 export default {
